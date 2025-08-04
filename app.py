c2ac381589582c3530caf50d20a98025f95eea29
ERROR404-hue <cristianjade99@gmail.com>
2025-08-04 18:51:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,3 +30,9 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+@app.route("/", methods=["GET"])
+def health_check():
+    print("*nudges sorry boss hard to keep awake!")
+    return "*nudges sorry boss hard to keep awake!", 200
+
